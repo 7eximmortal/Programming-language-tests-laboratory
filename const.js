247464139ccc94dbcec5b1203428dd526ca641f7
Vlad Petrenko <52258477+Pe1renko@users.noreply.github.com>
2021-10-17 21:10:44 +0700

    object

--- const.js
+++ const.js
@@ -1,25 +1,7 @@
-function Human(name, age, sex) {
-    this.name = name;
-    this.age = age;
-    this.sex = sex;
+let user = new function() {
+    this.name = `Petrenko`;
+    this.age = `21`;
+    this.sex = `Male`;
 };
 
-function NoHuman(name) {
-    let noThis = {};
-
-    noThis.name = name;
-
-    return noThis;
-};
-
-let maria = new Human(`Мария`, 16, `Женский`);
-let notMaria = NoHuman(`Не мария`);
-let notVlad = NoHuman(`Не влад`);
-
-console.log(notMaria.name);
-console.log(notVlad.name);
-
-console.log(`Все данные обьекта: 
-Имя: ${maria.name}
-Возраст: ${maria.age} 
-Пол: ${maria.sex}`);
+console.log(user);
