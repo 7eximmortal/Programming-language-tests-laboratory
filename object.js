2c02a254b672ed51c5245f36b315101781abbb16
Vlad Petrenko <52258477+Pe1renko@users.noreply.github.com>
2021-10-17 11:52:12 +0700

    upd

--- object.js
+++ object.js
@@ -1,11 +1,9 @@
 let user = {
     name: `Петренко`,
     age: 21,
-    hi() {return this.name},
+    hi: () => {
+        return this.name
+    },
 };
 
 console.log(user.hi())
-
-let ket = user.hi
-
-console.log(ket());
