fb8ffb31dabdfb18e631179470fa1ca0de3c32fe
Vlad Petrenko <52258477+Pe1renko@users.noreply.github.com>
2021-10-28 12:15:11 +0700

    solution to the problem: filtering unique array elements

--- prot.js
+++ prot.js
@@ -1,5 +1,17 @@
-let s = {
-    "vlad": `Привет!`,
+function unique(arr) {
+    let set = new Set(arr);
+    let names = [];
+
+    for (let value of set) {
+        names.push(value);
+    };
+    
+    return names;
 };
 
-console.log(s.vlad);
+let arr = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+
+console.log(unique(arr));
