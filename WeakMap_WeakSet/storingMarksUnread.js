d3bdb36faeb79b6a5bf08874171d46f6b7116c3a
Vlad Petrenko <52258477+Pe1renko@users.noreply.github.com>
2021-10-29 15:48:18 +0700

    123

--- WeakMap_WeakSet/storingMarksUnread.js
+++ WeakMap_WeakSet/storingMarksUnread.js
@@ -1,27 +1,13 @@
 read = new WeakSet();
 
-function readAddMsg(obj) {
-    read.add(obj);
-};
-
-function readHasMsg(obj) {
-    let reading = read.has(obj);
-
-    if (reading) {
-        return `Прочитано!`;
-    } else {
-        return `Не прочитано!`;
-    };
-};
-
 let messages = [
     {text: "Hello", from: "John"},
     {text: "How goes?", from: "John"},
     {text: "See you soon", from: "Alice"}
 ];
 
-readAddMsg(messages[0]);
+read.add(messages[0]);
 
 messages[0] = null;
 
-console.log(readHasMsg(messages[0]));
+console.log(read.has(messages[0]));
